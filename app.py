a65e6665f956762cdc50a1e55f487e22e8b5160a
my 666 demons <89980544+enamorak@users.noreply.github.com>
2025-05-28 10:50:28 +0300

    lovely front

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string, send_file
+from flask import Flask, request, render_template, send_file
 import os
 from werkzeug.utils import secure_filename
 from crypto import Encrypt, Decrypt
@@ -10,42 +10,6 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 CIPHERS = ['aes', 'rsa2048', 'rsa4096', 'des']
 
-HTML_FORM = """
-<!doctype html>
-<title>File Encryption / Decryption</title>
-<h2>Загрузите файл для шифрования или расшифровки</h2>
-<form method=post enctype=multipart/form-data>
-  <label>Файл:</label><br>
-  <input type=file name=file required><br><br>
-  
-  <label>Файл ключа (только для расшифровки):</label><br>
-  <input type=file name=keyfile><br><br>
-  
-  <label for=cipher>Выберите шифр:</label>
-  <select name=cipher required>
-    {% for c in ciphers %}
-      <option value="{{c}}">{{c.upper()}}</option>
-    {% endfor %}
-  </select><br><br>
-  
-  <button type="submit" name="action" value="encrypt">Зашифровать</button>
-  <button type="submit" name="action" value="decrypt">Расшифровать</button>
-</form>
-
-{% if result %}
-  <h3>Скачайте файлы:</h3>
-  {% if result.enc %}
-    <a href="{{ url_for('download_file', filename=result.enc) }}">Скачать шифрофайл</a><br>
-  {% endif %}
-  {% if result.key %}
-    <a href="{{ url_for('download_file', filename=result.key) }}">Скачать {{ result.key }}</a><br>
-  {% endif %}
-  {% if result.dec %}
-    <a href="{{ url_for('download_file', filename=result.dec) }}">Скачать расшифрованный файл</a><br>
-  {% endif %}
-{% endif %}
-"""
-
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     result = None
@@ -53,7 +17,6 @@ def upload_file():
         action = request.form.get('action')
         cipher = request.form['cipher'].lower()
 
-        # Сохраняем основной файл
         uploaded_file = request.files['file']
         filename = secure_filename(uploaded_file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
@@ -85,7 +48,7 @@ def upload_file():
         else:
             return "Неизвестное действие", 400
 
-    return render_template_string(HTML_FORM, ciphers=CIPHERS, result=result)
+    return render_template('index.html', ciphers=CIPHERS, result=result)
 
 @app.route('/uploads/<filename>')
 def download_file(filename):
@@ -93,4 +56,3 @@ def download_file(filename):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
